e419676f16982cb5573fd5abb5c8cf493a0e8f59
AriEncarnacion <ari.encarnacion.95@gmail.com>
2024-05-13 03:34:41 -0700

    middleware approach for CORS

--- app/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server"
-import { updateSession } from "./lib"
-
-export async function middleware(request: NextRequest) {
-  console.log("middleware::request::", request)
-  return await updateSession(request)
-}
